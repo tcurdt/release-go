ef45a1f6b4f7b91bab92b2fdcbca6d00263c4706
Torsten Curdt <tcurdt@vafer.org>
2023-11-01 02:17:21 +0100

    parse cli

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
+var (
+	version bool
+	port    *int
+)
+
+func init() {
+	flag.BoolVar(&version, "v", false, "print version")
+	port = flag.Int("port", 2015, "port number")
+}
+
 func main() {
+
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if version {
+		fmt.Println("Version 1.0")
+		return
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	http.ListenAndServe(":80", nil)
+	fmt.Printf("Listening on port %d\n", *port)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
