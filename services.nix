2b9cd906024d2de00f8a51f6eaa3441aa13e9782
Torsten Curdt <tcurdt@vafer.org>
2024-03-02 23:53:55 +0000

    service cmd

--- services.nix
+++ services.nix
@@ -27,11 +27,12 @@ in
 
       serviceConfig = {
         DynamicUser = true;
-        ExecStart = "${self.packages.${pkgs.system}.default}/bin/release-go --config ${configFile}";
+        ExecStart = "${self.packages.${pkgs.system}.default}/bin/release-go";
+        # ExecStart = "${self.packages.${pkgs.system}.default}/bin/release-go --config ${configFile}";
         Restart = "on-failure";
 
-        AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
-        CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+        #AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+        #CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
       };
     };
   };
