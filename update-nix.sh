f10898e1d94ed682e7e4afbd825880bcc7ac4755
Torsten Curdt <tcurdt@vafer.org>
2024-06-04 00:12:21 +0200

    simplified

--- update-nix.sh
+++ update-nix.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -xe
+
 FILE="default.nix"
 
-sed -i '' -e "s/__VERSION__/$(git describe --tags --abbrev=0)/g" "$FILE"
+VERSION=$(git describe --tags --abbrev=0)
+
+sed -e "s/version = \".*\";/version = \"${VERSION}\";/g" $FILE > $FILE.new && mv $FILE.new $FILE
 
 nix-build -E "with import <nixpkgs> { }; callPackage ./$FILE { }"
+
 SRC_SHA256=$(nix-build -E "with import <nixpkgs> { }; callPackage ./$FILE { }" 2>&1 | grep -oE 'got:\s+sha256-\S+' | cut -d "-" -f 2)
-sed -i '' -e "s|sha256 = \"\";|sha256 = \"$SRC_SHA256\";|g" "$FILE"
+sed -e "s|sha256 = \"\";|sha256 = \"$SRC_SHA256\";|g" $FILE > $FILE.new && mv $FILE.new $FILE
 
 VENDOR_SHA256=$(nix-build -E "with import <nixpkgs> { }; callPackage ./$FILE { }" 2>&1 | grep -oE 'got:\s+sha256-\S+' | cut -d "-" -f 2)
-sed -i '' -e "s|vendorHash = \"\";|vendorHash = \"sha256-$VENDOR_SHA256\";|g" "$FILE"
+sed -e "s|vendorHash = \"\";|vendorHash = \"sha256-$VENDOR_SHA256\";|g" $FILE > $FILE.new && mv $FILE.new $FILE
